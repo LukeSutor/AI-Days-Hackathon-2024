169800a7930bada088e1d910cab80e8a7fe1af06
Luke Sutor <lukesutor@gmail.com>
2024-10-25 18:51:47 -0400

    add earth images

--- web/src/App.js
+++ web/src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import Globe from "react-globe.gl";
 
+function handleGlobeClick(e) {
+  console.log(e);
+}
+
 function App() {
   return (
     <div>
       <div className="flex flex-row h-full">
-        <div className="w-1/2 bg-black">
-          <Globe />
-        </div>
+        <Globe 
+            globeImageUrl="./earth.jpg"
+            bumpImageUrl="./heightmap.png"
+            onGlobeClick={handleGlobeClick}/>
         <div className="w-1/2">
 
         </div>
--- web/src/components/Globe.js
+++ web/src/components/Globe.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Globe from 'react-globe.gl';
+import earth from '../../public/earth.jpg'
 
 
 const Globe = () => {
     return (
         <div>
-            <Globe 
-            globeImageUrl="./earth.jpg"/>
+            {/* <Globe 
+            globeImageUrl="./earth.jpg"/> */}
+            <img src={earth} />
         </div>
     );
 }
