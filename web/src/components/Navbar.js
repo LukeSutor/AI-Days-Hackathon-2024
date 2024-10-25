4bf82235ad1a91c6a7455899bb128a2fa7523733
Luke Sutor <lukesutor@gmail.com>
2024-10-25 18:26:11 -0400

    add navbar and tailwind

---
+++ web/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <text className='text-4xl'>NAV</text>
+        </div>
+    );
+}
+
+
+export default Navbar
