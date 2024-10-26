f38fa4e321aff04b4e0d11dcba3f961d5156184b
Michael Carroll <michaelc32204@gmail.com>
2024-10-25 20:10:17 -0400

    axios

--- web/src/App.js
+++ web/src/App.js
@@ -1,17 +1,35 @@
 import React from "react";
 import Globe from "react-globe.gl";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function handleGlobeClick(e) {
   console.log(e);
 }
 
 function App() {
+
+  const [markers, setMarkers] = useState([]);
+  // test
+  useEffect(() => {
+    // load data
+    axios.get('https://www.fema.gov/api/open/v1/FemaWebDisasterDeclarations')
+      .then((res) => {
+        const { features } = res.data; 
+        setMarkers(features);
+      })
+      .catch((err) => console.error("Error fetching data:", err));
+  }, []);
+
+
+
+  const weightColor = [];
+
+
   return (
     <div>
       <div className="flex flex-row h-full">
-        <Globe 
-            globeImageUrl="./earth.jpg"
-            bumpImageUrl="./heightmap.png"
+        <Globe
             onGlobeClick={handleGlobeClick}/>
         <div className="w-1/2">
 
